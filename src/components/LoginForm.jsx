7c38ba7b7053f8c46622a1961aeea5fb439013f9
Marcos Simon <marcosht503@gmail.com>
2024-04-27 17:20:58 -0400

    Added form and loading state to Login Inputs

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { TextField, Button, Grid } from '@mui/material';
-import { auth } from '../config/firebaseConfig'; 
+import { auth } from '../config/firebaseConfig';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
@@ -45,55 +45,76 @@ const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
-  const handleLogin = async () => {
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
     try {
-      await signInWithEmailAndPassword(auth,email, password); 
+      await signInWithEmailAndPassword(auth, email, password);
       navigate('/');
     } catch (error) {
-      setError(error.message);
+      console.error(error);
+      setError('Correo electrónico o contraseña incorrectos');
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <Grid container direction="column" spacing={3} alignItems="center">
       <ThemeProvider theme={customTheme(outerTheme)}>
-      <TextField
-        label="Correo electrónico"
-        variant="outlined"
-        fullWidth
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <TextField
-        label="Contraseña"
-        variant="outlined"
-        type="password"
-        fullWidth
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        style={{ marginTop: 15 }}
-      />
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <Button
-        variant="contained"
-        fullWidth
-        sx={{
-          backgroundColor: "#47361A",
-          color: "#F7C677",
-          paddingTop: 2,
-          paddingBottom: 2,
-          marginTop: 5,
-          "&:hover": {
-            backgroundColor: "#705528", 
-            
-          },
-        }}
-        onClick={handleLogin}
-      >
-        Iniciar sesión
-      </Button>
+        <form className="w-full" onSubmit={handleLogin}>
+          <TextField
+            label="Correo electrónico"
+            variant="outlined"
+            fullWidth
+            required
+            type="email"
+            value={email}
+            onChange={
+              (e) => {
+                setError(null);
+                setEmail(e.target.value)
+              }}
+          />
+          <TextField
+            label="Contraseña"
+            variant="outlined"
+            type="password"
+            fullWidth
+            required
+            value={password}
+            onChange={(e) => {
+              setError(null);
+              setPassword(e.target.value)
+            }}
+            style={{ marginTop: 15 }}
+          />
+          {error && <p style={{ color: 'red', fontSize: '1rem', textAlign:"center" }}>{error}</p>}
+        <Button
+          disabled={isLoading}
+          variant="contained"
+          type="submit"
+          fullWidth
+          sx={{
+            backgroundColor: "#47361A",
+            color: "#F7C677",
+            paddingTop: 2,
+            paddingBottom: 2,
+            marginTop: 5,
+            "&:hover": {
+              backgroundColor: "#705528",
+
+            },
+          }}
+          
+        >
+          {isLoading ? 'Cargando...' : 'Iniciar sesión'}
+        </Button>
+        </form>
+        
       </ThemeProvider>
     </Grid>
   );

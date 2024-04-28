175a6336a4cef68832c94741e7f664f219181e32
Marcos Simon <marcosht503@gmail.com>
2024-04-28 15:16:55 -0400

    Connected shelter page to backend to fetch shelters from database

--- src/pages/SheltersPage.jsx
+++ src/pages/SheltersPage.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import ShelterCard from '../components/ShelterCard'
+import { backendAPI } from '../config/axiosConfig'
 
 const SheltersPage = () => {
+  const [shelters, setShelters] = useState([])
+  useEffect(() => {
+    backendAPI.get("/shelter").then((response) => {
+        setShelters(response.data)
+    }).catch((error) => {
+      console.error(error)
+    })
+
+  },[])  
+  
   return (
+    
     <div className="bg-primary min-h-screen flex flex-col justify-start items-center">
         <h1 className="font-montserrat text-title text-third font-bold text-center">Refugios</h1>
         <div className="flex flex-row flex-wrap justify-center items-center gap-[1rem] px-[2rem]">
-            <ShelterCard name="Happy Paws Whiskers Shelter" location="Pawsitively Purrfect Shelter" phone="1234567890" />
-            <ShelterCard name="Paws and Claws Shelter" location="456 Elm St, London" phone="0987654321" />
-            <ShelterCard name="Furry Friends Shelter" location="789 Oak St, Sydney" phone="6789012345" />
-            <ShelterCard name="Whiskers and Wags Shelter" location="202 Maple St, Mexico City" phone="5432167890" />
-            <ShelterCard name="Pawsitively Purrfect Shelter" location="101 Pine St, Mumbai" phone="0987654321" />
+            {shelters.length > 0 ? shelters.map((shelter) => (
+                <ShelterCard key={shelter.id} name={shelter.name} location={shelter.location} phone={`+${shelter.countryCode}-${shelter.phone}`} image={shelter.photo} />
+            )) : <p className="font-montserrat text-title text-third font-bold text-center">CARGANDO...</p>}
             
         </div>
 

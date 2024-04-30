dd2e5e7818e51f8552d025e53e740c881c43cb26
Marcos Simon <marcosht503@gmail.com>
2024-04-30 19:12:54 -0400

    Replaced hardcorded axios URL with env variable

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const backendAPI = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: process.env.REACT_APP_BACKEND_URL,
     headers: {
         "Content-Type": "application/json",
     },

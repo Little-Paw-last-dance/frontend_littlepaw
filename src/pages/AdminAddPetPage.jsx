9559bb5939a71c10e574633f36fa5f6f8e60a1e1
Marcos <marcosht503@gmail.com>
2024-05-02 10:52:24 -0400

    Added documentation for AdminAddPetPage

--- src/pages/AdminAddPetPage.jsx
+++ src/pages/AdminAddPetPage.jsx
@@ -3,6 +3,16 @@ import { Container, Typography } from '@mui/material';
 import AddPetForm from '../components/AddPetForm';
 
 const AdminAddPetPage = () => {
+  /**
+   * Página para añadir una mascota a la base de datos como administrador. Exlcusivo para añadir mascotas a refugios.
+   * @requires AddPetForm
+   * 
+   * @returns {React.Component} Página de añadir mascota como administrador.
+   * 
+   * @example
+   * // Ejemplo de uso:
+   * <AdminAddPetPage />
+   */
   return (
     <div className="bg-primary min-h-screen flex flex-col justify-center items-center">
       <Typography variant="h2" align="center" gutterBottom fontWeight="bold" 

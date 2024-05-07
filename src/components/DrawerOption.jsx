6a32358752ee89e5a7f7fce74f799ce6b6809ad3
Marcos Simon <marcosht503@gmail.com>
2024-05-07 12:32:43 -0400

    Added documentation for DrawerOption

--- src/components/DrawerOption.jsx
+++ src/components/DrawerOption.jsx
@@ -4,6 +4,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Box } from '@mui/material'
 import {useNavigate} from 'react-router-dom'
 
+/**
+ * Componente de opción de menú para el componente de DrawerMenu
+ * 
+ * @component
+ * @param {IconDefinition} icon - Icono de FontAwesome a mostrar
+ * @param {String} content - Contenido de la opción
+ * @param {String} path - Ruta a la que redirigir al hacer click
+ * @param {Function} action - Función a ejecutar al hacer click, solo se ejecuta si el usuario desea cerrar sesión
+ * @returns {React.Component} Componente de opción de menú
+ * 
+ * @example
+ * // Ejemplo de uso:
+ * <DrawerOption icon={faPaw} content="AÑADIR MASCOTA" path="/addpet"/>
+ *
+ * 
+ * 
+ * 
+ */
+
 const DrawerOption = ({icon, content, path, action}) => {
     const navigate = useNavigate()
   return (
